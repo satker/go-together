df1a8f3cb11b37233f8927ad4ce4b578ee6c5e95
satker <satker73@gmail.com>
2020-04-22 16:24:24 +0300

    fixed image

--- frontend/src/forms/Event/View/Messages/Chats/MessageItem.js
+++ frontend/src/forms/Event/View/Messages/Chats/MessageItem.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import {getSrcForImg} from "../../../../utils/utils";
-import {Image} from "react-bootstrap";
 
 const MessageItem = ({message, user, userMessageId, setUserMessageId}) => {
     return <div className='message-item custom-border' onClick={() => setUserMessageId(user.id)}
                 style={{borderRightStyle: userMessageId === user.id ? 'none' : ''}}>
         <div className='flex' style={{width: '30%'}}>
-            <Image className='simple_user_img' src={getSrcForImg(user.userPhoto)}/>
+            <img className='simple_user_img' src={getSrcForImg(user.userPhoto)} alt=''/>
         </div>
         <div className='flex' style={{width: '70%'}}>
             <div className='flex'>{user.login}</div>
--- frontend/src/forms/utils/components/CardItems/Items/SimpleUserCard.js
+++ frontend/src/forms/utils/components/CardItems/Items/SimpleUserCard.js
@@ -1,13 +1,12 @@
 import React from "react";
 import {CardSubtitle, CardTitle} from "reactstrap";
-import {Image} from "react-bootstrap";
 import {getSrcForImg} from "../../../utils";
 import {SimpleUser} from "../../../types";
 
 const SimpleUserCard = ({user}) => {
     return <div className='simple-user-item form-group'>
         <div className='flex'>
-            <Image className='simple_user_img' src={getSrcForImg(user.userPhoto)}/>
+            <img className='simple_user_img' src={getSrcForImg(user.userPhoto)} alt=''/>
         </div>
         <div className='flex'>
             <CardTitle>Login: {user.login}</CardTitle>
