b303ed077e8a1f7a2c449656e8a061f5330a8a85
satker <satker73@gmail.com>
2020-05-16 10:14:21 +0300

    fixed new message error

--- frontend/src/forms/Event/View/Messages/reducers.js
+++ frontend/src/forms/Event/View/Messages/reducers.js
@@ -1,8 +1,9 @@
 import {createEmptyResponse} from "../../../../App/utils/utils";
-import {MESSAGES_CHATS, MESSAGES_MESSAGES_BY_EVENT, MESSAGES_USERS_INFO} from "./constants";
+import {MESSAGES_CHATS, MESSAGES_MESSAGES_BY_EVENT, MESSAGES_NEW_MESSAGE, MESSAGES_USERS_INFO} from "./constants";
 
 export const messages = {
     messagesByEvent: createEmptyResponse(MESSAGES_MESSAGES_BY_EVENT, []),
     chats: createEmptyResponse(MESSAGES_CHATS, []),
-    usersInfo: createEmptyResponse(MESSAGES_USERS_INFO, [])
+    usersInfo: createEmptyResponse(MESSAGES_USERS_INFO, []),
+    newMessage: createEmptyResponse(MESSAGES_NEW_MESSAGE, {})
 };
