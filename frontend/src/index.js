701296e7103e0d2fbc6f11d5afc816ddc3573544
satker <satker73@gmail.com>
2020-05-19 09:04:35 +0300

    added worked webconfig

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'index.css';
 import App from 'App';
-//import registerServiceWorker from './RegisterServiceWorker';
+
+import unregister from './RegisterServiceWorker';
+
+import './index.css';
+
 
 ReactDOM.render(
     <App/>, document.getElementById('root'));
-//registerServiceWorker();
+
+unregister();
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,6 +1,27 @@
 const path = require('path');
 
 module.exports = {
+    mode: 'development',
+    entry: __dirname + '/src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    },
     resolve: {
         alias: {
             App: path.resolve(__dirname, '/Users/kunats-ay/IdeaProjects/go-together/frontend/src/App'),
